36be2db2e6f7801ceb799a8d767a656dc25c32f2
Albin Vass <albinvass@gmail.com>
2023-12-01 19:43:01 +0100

    Add i3

--- flake.nix
+++ flake.nix
@@ -3,10 +3,30 @@
     nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
     home-manager.url = "github:nix-community/home-manager";
   };
-  outputs = { self, nixpkgs, ... }@attrs: {
-    nixosConfigurations.albinvass-dellxps = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./laptop/configuration.nix ];
+  outputs = { self, nixpkgs, ... }@attrs:
+  let
+    system = "x86_64-linux";
+    pkgs = import  nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
     };
+  in {
+    nixosConfigurations.albinvass-dellxps = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./laptop/configuration.nix
+	self.nixosModule.i3
+      ];
+    };
+    nixosModule =
+      let
+        lib = nixpkgs.lib;
+        filterDirectories = (files: lib.attrsets.filterAttrs (name: type: type == "directory") files);
+        getDirectories = d: builtins.attrNames (filterDirectories (builtins.readDir d));
+        createNixosModule = name: {
+          ${name} = import ./modules/${name};
+        };
+        modules = map createNixosModule (getDirectories ./modules);
+      in lib.attrsets.mergeAttrsList modules;
   };
 }
---
+++ modules/i3/default.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+{
+  config = {
+    services.xserver.windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+    };
+
+    programs.i3lock.enable = true;
+  };
+}
