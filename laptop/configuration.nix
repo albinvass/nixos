96de013292eea47386db4423ff6e21dd1d8a9e94
Albin Vass <albinvass@gmail.com>
2023-12-16 19:12:35 +0100

    Add compat for /bin/bash etc.

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -44,6 +44,9 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  # Add compatibility for /bin/bash etc.
+  services.envfs.enable = true;
+
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
