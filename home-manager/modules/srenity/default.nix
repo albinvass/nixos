b04208c31c2a035382a312531c67a90f1e904d13
Albin Vass <albinvass@gmail.com>
2023-12-21 09:52:13 +0100

    Move srenity secrets to specific secrets

---
+++ home-manager/modules/srenity/default.nix
@@ -0,0 +1,12 @@
+{ lib, ... }: {
+  sops = {
+    secrets = lib.attrsets.mapAttrs' (k: v:
+      lib.attrsets.nameValuePair
+        ("srenity/${k}")
+        (v // { sopsFile = ./secrets.yaml; })
+    ) {
+      "OS_USERNAME" = {};
+      "OS_PASSWORD" = {};
+    };
+  };
+}
