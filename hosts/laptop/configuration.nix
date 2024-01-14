e37f477246e7ef18848cf854a6afad383009c5dc
Albin Vass <albinvass@gmail.com>
2024-01-14 21:41:38 +0100

    Add cachix caches

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -8,7 +8,19 @@
   system.stateVersion = "23.05"; # Did you read the comment?
   nix = {
     package = pkgs.nix;
-    settings.experimental-features = [ "nix-command" "flakes" ];
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      extra-substituters = [
+        "https://albinvass.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://numtide.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "albinvass.cachix.org-1:9BoW/lEIrRQQt7rnktA3qOg9e9G/Fa2SNwqnUEKq8aM="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      ];
+    };
   };
   imports =
     [ # Include the results of the hardware scan.
