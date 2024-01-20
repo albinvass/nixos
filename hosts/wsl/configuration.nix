95b4012b427bb15b36f279e3e39ffe4c42e169ca
Albin Vass <albinvass@gmail.com>
2024-01-20 17:36:48 +0000

    Fix nix conf for wsl

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -13,6 +13,22 @@
     shell = pkgs.zsh;
   };
   
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      extra-substituters = [
+        "https://albinvass.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://numtide.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "albinvass.cachix.org-1:9BoW/lEIrRQQt7rnktA3qOg9e9G/Fa2SNwqnUEKq8aM="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      ];
+    };
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
