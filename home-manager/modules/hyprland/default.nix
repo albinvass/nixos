0fd17902def41e8e69b9b6a729877c0796f7dd61
Albin Vass <albinvass@gmail.com>
2024-02-09 16:34:26 +0100

    Move hyprland config to .settings instaed of extraConfig

--- home-manager/modules/hyprland/default.nix
+++ home-manager/modules/hyprland/default.nix
@@ -29,6 +29,161 @@
       hyprgrass.packages.${pkgs.system}.default
       split-monitor-workspaces.packages.${pkgs.system}.split-monitor-workspaces
     ];
+
+    settings = {
+      "$mainMod" = "SUPER";
+      "$altMod" = "SUPERALT";
+
+      source = [ "~/.config/hypr/monitors.conf" ];
+      exec-once = [
+        "waybar"
+        "swww init"
+        "[workspace 1 silent] konsole"
+        "[workspace 2 silent] konsole"
+        "[workspace 3 silent; group] microsoft-edge"
+
+        "[workspace 5 silent; group] slack"
+        "[workspace 5 silent; group] discord"
+        "[workspace 5 silent; group] element-desktop"
+        "[workspace 5 silent; group] signal-desktop"
+      ];
+      input = {
+        kb_layout = "se";
+        follow_mouse = 1;
+        touchpad = {
+            natural_scroll = "no";
+        };
+        sensitivity = 0;
+      };
+      general = {
+        border_size = 1;
+      };
+      misc = {
+        disable_hyprland_logo = true;
+      };
+      decoration = {
+        rounding = 6;
+      };
+      dwindle = {
+        preserve_split = true;
+      };
+      group = {
+        groupbar = {
+          font_family = "Ubuntu Nerd Font";
+          font_size	 = 20;
+        };
+      };
+      gestures = {
+        workspace_swipe = true;
+        workspace_swipe_distance = 400;
+        workspace_swipe_min_speed_to_force = 50;
+        workspace_swipe_cancel_ratio = 0.10;
+        workspace_swipe_invert = false;
+      };
+      plugin = {
+        split-monitor-workspaces = {
+          count = 10;
+        };
+
+        touch_gestures = {
+          # The default sensitivity is probably too low on tablet screens,
+          # I recommend turning it up to 4.0
+          sensitivity = 4.0;
+
+          # must be >= 3
+          workspace_swipe_fingers = 3;
+
+          experimental = {
+            # send proper cancel events to windows instead of hacky touch_up events,
+            # NOT recommended as it crashed a few times, once it's stabilized I'll make it the default
+            send_cancel = 0;
+          };
+        };
+      };
+      windowrulev2 = [
+        "workspace silent 5,class:^Slack$"
+        "group,class:^Slack$"
+        "workspace silent 5,class:^discord$"
+        "group,class:^discord$"
+        "workspace silent 5,class:^Element$"
+        "group,class:^Element$"
+        "workspace silent 5,class:^Signal$"
+        "group,class:^Signal$"
+      ];
+      bind = [
+        "$mainMod, Return, exec, konsole"
+        "$altMod, 1, exec, wezterm ssh avass@65.108.153.140"
+        "$mainMod_SHIFT, Q, killactive,"
+        "$mainMod_SHIFT, E, exit,"
+        "$mainMod, D, exec, rofi -show drun"
+        "$mainMod_SHIFT, D, exec, rofimoji"
+        "$mainMod, C, exec, rofi -show window"
+        "$mainMod, F, fullscreen, 1"
+        "$mainMod, S, togglegroup"
+        "$mainMod, U, moveoutofgroup"
+        "$mainMod, P, pseudo," # dwindle
+        "$mainMod, V, togglesplit," # dwindle
+
+        # Move focus with mainMod + vim keys
+        "$mainMod, h, movefocus, l"
+        "$mainMod, j, movefocus, d"
+        "$mainMod, k, movefocus, u"
+        "$mainMod, l, movefocus, r"
+        "$mainMod, x, changegroupactive, f"
+        "$mainMod, z, changegroupactive, b"
+
+        # Move window with mainMod + vim keys
+        "$mainMod_SHIFT, h, movewindow, l"
+        "$mainMod_SHIFT, j, movewindow, d"
+        "$mainMod_SHIFT, k, movewindow, u"
+        "$mainMod_SHIFT, l, movewindow, r"
+
+        # Switch workspaces with mainMod + [0-9]
+        "$mainMod, 1, split-workspace, 1"
+        "$mainMod, 2, split-workspace, 2"
+        "$mainMod, 3, split-workspace, 3"
+        "$mainMod, 4, split-workspace, 4"
+        "$mainMod, 5, split-workspace, 5"
+        "$mainMod, 6, split-workspace, 6"
+        "$mainMod, 7, split-workspace, 7"
+        "$mainMod, 8, split-workspace, 8"
+        "$mainMod, 9, split-workspace, 9"
+        "$mainMod, 0, split-workspace, 10"
+
+        # Move active window to a workspace with mainMod + SHIFT + [0-9]
+        "$mainMod SHIFT, 1, split-movetoworkspace, 1"
+        "$mainMod SHIFT, 2, split-movetoworkspace, 2"
+        "$mainMod SHIFT, 3, split-movetoworkspace, 3"
+        "$mainMod SHIFT, 4, split-movetoworkspace, 4"
+        "$mainMod SHIFT, 5, split-movetoworkspace, 5"
+        "$mainMod SHIFT, 6, split-movetoworkspace, 6"
+        "$mainMod SHIFT, 7, split-movetoworkspace, 7"
+        "$mainMod SHIFT, 8, split-movetoworkspace, 8"
+        "$mainMod SHIFT, 9, split-movetoworkspace, 9"
+        "$mainMod SHIFT, 0, split-movetoworkspace, 10"
+
+        # Special workspace
+        "$mainMod, comma, togglespecialworkspace, magic"
+        "$mainMod SHIFT, comma, movetoworkspace, special:magic"
+
+        # Scroll through existing workspaces with mainMod + scroll
+        "$mainMod, mouse_down, workspace, e+1"
+        "$mainMod, mouse_up, workspace, e-1"
+
+        # Brightness, Audio etc
+        ", XF86MonBrightnessDown, exec, brillo -q -U 5"
+        ", XF86MonBrightnessUp, exec, brillo -q -A 5"
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+      ];
+      bindm = [
+        # Move/resize windows with mainMod + LMB/RMB and dragging
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+    };
   };
 
   services.mako = {
@@ -43,101 +198,6 @@
   };
 
   wayland.windowManager.hyprland.extraConfig = ''
-    source = ~/.config/hypr/monitors.conf
-    exec-once=polkit-agent-helper-1
-    exec-once=waybar
-    exec-once=swww init
-    input {
-      kb_layout = se
-      kb_variant =
-      kb_model =
-      kb_options =
-      kb_rules =
-
-      follow_mouse = 1
-
-      touchpad {
-          natural_scroll = no
-      }
-
-      sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
-
-    }
-
-    general {
-      border_size = 1
-    }
-
-    misc {
-      disable_hyprland_logo = true
-    }
-
-    decoration {
-      rounding = 6
-      col.active_border 0xff444444
-    }
-
-    dwindle {
-      preserve_split = true
-    }
-
-    group {
-      groupbar {
-        font_family = "Ubuntu Nerd Font"
-        font_size	 = 20
-      }
-    }
-
-
-    windowrulev2 = workspace silent 5,class:^Slack$
-    windowrulev2 = group,class:^Slack$
-    windowrulev2 = workspace silent 5,class:^discord$
-    windowrulev2 = group,class:^discord$
-    windowrulev2 = workspace silent 5,class:^Element$
-    windowrulev2 = group,class:^Element$
-    windowrulev2 = workspace silent 5,class:^Signal$
-    windowrulev2 = group,class:^Signal$
-
-    exec-once = [workspace 1 silent] konsole
-    exec-once = [workspace 2 silent] konsole
-    exec-once = [workspace 3 silent; group] microsoft-edge
-
-    exec-once = [workspace 5 silent; group] slack
-    exec-once = [workspace 5 silent; group] discord
-    exec-once = [workspace 5 silent; group] element-desktop
-    exec-once = [workspace 5 silent; group] signal-desktop
-
-
-    $mainMod = SUPER
-    $altMod = SUPERALT
-    bind = $mainMod, Return, exec, konsole
-    bind = $altMod, 1, exec, wezterm ssh avass@65.108.153.140
-    bind = $mainMod_SHIFT, Q, killactive,
-    bind = $mainMod_SHIFT, E, exit,
-    bind = $mainMod, D, exec, rofi -show drun
-    bind = $mainMod_SHIFT, D, exec, rofimoji
-    bind = $mainMod, C, exec, rofi -show window
-    bind = $mainMod, F, fullscreen, 1
-    bind = $mainMod, S, togglegroup
-    bind = $mainMod, U, moveoutofgroup
-    bind = $mainMod, P, pseudo, # dwindle
-    bind = $mainMod, V, togglesplit, # dwindle
-
-
-    # Move focus with mainMod + vim keys
-    bind = $mainMod, h, movefocus, l
-    bind = $mainMod, j, movefocus, d
-    bind = $mainMod, k, movefocus, u
-    bind = $mainMod, l, movefocus, r
-    bind = $mainMod, x, changegroupactive, f
-    bind = $mainMod, z, changegroupactive, b
-
-    # Move window with mainMod + vim keys
-    bind = $mainMod_SHIFT, h, movewindow, l
-    bind = $mainMod_SHIFT, j, movewindow, d
-    bind = $mainMod_SHIFT, k, movewindow, u
-    bind = $mainMod_SHIFT, l, movewindow, r
-
     # will switch to a submap called resize
     bind = $mainMod, R, submap, resize
     submap = resize
@@ -146,92 +206,6 @@
     binde = , l, resizeactive, 10 0
     binde = , k, resizeactive, 0 -10
     bind = , escape, submap, reset
-    submap=reset
-
-    # Switch monitor with mainMod + CTRL + [0-9]
-    # TODO: needs a script
-    #bind = $mainMod CTRL, 1, focusmonitor, 1
-    #bind = $mainMod CTRL, 2, focusmonitor, 2
-    #bind = $mainMod CTRL, 3, focusmonitor, 3
-    #bind = $mainMod CTRL, 4, focusmonitor, 4
-    #bind = $mainMod CTRL, 5, focusmonitor, 5
-    #bind = $mainMod CTRL, 6, focusmonitor, 6
-    #bind = $mainMod CTRL, 7, focusmonitor, 7
-    #bind = $mainMod CTRL, 8, focusmonitor, 8
-    #bind = $mainMod CTRL, 9, focusmonitor, 9
-    #bind = $mainMod CTRL, 0, focusmonitor, 10
-
-    # Switch workspaces with mainMod + [0-9]
-    bind = $mainMod, 1, split-workspace, 1
-    bind = $mainMod, 2, split-workspace, 2
-    bind = $mainMod, 3, split-workspace, 3
-    bind = $mainMod, 4, split-workspace, 4
-    bind = $mainMod, 5, split-workspace, 5
-    bind = $mainMod, 6, split-workspace, 6
-    bind = $mainMod, 7, split-workspace, 7
-    bind = $mainMod, 8, split-workspace, 8
-    bind = $mainMod, 9, split-workspace, 9
-    bind = $mainMod, 0, split-workspace, 10
-
-    # Move active window to a workspace with mainMod + SHIFT + [0-9]
-    bind = $mainMod SHIFT, 1, split-movetoworkspace, 1
-    bind = $mainMod SHIFT, 2, split-movetoworkspace, 2
-    bind = $mainMod SHIFT, 3, split-movetoworkspace, 3
-    bind = $mainMod SHIFT, 4, split-movetoworkspace, 4
-    bind = $mainMod SHIFT, 5, split-movetoworkspace, 5
-    bind = $mainMod SHIFT, 6, split-movetoworkspace, 6
-    bind = $mainMod SHIFT, 7, split-movetoworkspace, 7
-    bind = $mainMod SHIFT, 8, split-movetoworkspace, 8
-    bind = $mainMod SHIFT, 9, split-movetoworkspace, 9
-    bind = $mainMod SHIFT, 0, split-movetoworkspace, 10
-
-
-    # Example special workspace (scratchpad)
-    bind = $mainMod, comma, togglespecialworkspace, magic
-    bind = $mainMod SHIFT, comma, movetoworkspace, special:magic
-
-    # Scroll through existing workspaces with mainMod + scroll
-    bind = $mainMod, mouse_down, workspace, e+1
-    bind = $mainMod, mouse_up, workspace, e-1
-
-    # Move/resize windows with mainMod + LMB/RMB and dragging
-    bindm = $mainMod, mouse:272, movewindow
-    bindm = $mainMod, mouse:273, resizewindow
-
-    # Brightness, Audio etc
-    bind=,XF86MonBrightnessDown, exec, brillo -q -U 5
-    bind=,XF86MonBrightnessUp, exec, brillo -q -A 5
-    bind=,XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%+
-    bind=,XF86AudioLowerVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%-
-    bind=,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-    bind=,XF86AudioPlay, exec, playerctl play-pause
-
-    gestures {
-      workspace_swipe = true
-      workspace_swipe_distance = 400
-      workspace_swipe_min_speed_to_force = 50
-      workspace_swipe_cancel_ratio = 0.10
-      workspace_swipe_invert = false
-    }
-    plugin {
-      split-monitor-workspaces {
-        count = 10
-      }
-
-      touch_gestures {
-        # The default sensitivity is probably too low on tablet screens,
-        # I recommend turning it up to 4.0
-        sensitivity = 4.0
-
-        # must be >= 3
-        workspace_swipe_fingers = 3
-
-        experimental {
-          # send proper cancel events to windows instead of hacky touch_up events,
-          # NOT recommended as it crashed a few times, once it's stabilized I'll make it the default
-          send_cancel = 0
-        }
-      }
-    }
+    submap = reset
   '';
 }
