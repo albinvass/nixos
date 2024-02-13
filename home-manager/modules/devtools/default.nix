9e9dd710b4855c7675d2d0cde8c0ca5a0d3b17ee
Albin Vass <albinvass@gmail.com>
2024-02-13 21:58:14 +0100

    Move development tools to devtools

--- home-manager/modules/devtools/default.nix
+++ home-manager/modules/devtools/default.nix
@@ -1,4 +1,4 @@
-{ inputs, homeManagerModules, ... }:
+{ pkgs, inputs, homeManagerModules, ... }:
 {
   imports = [
     homeManagerModules.neovim
@@ -9,5 +9,55 @@
     homeManagerModules.sops
     homeManagerModules.srenity
     inputs.sops-nix.homeManagerModules.sops
+  ];
+
+  programs = {
+    htop.enable = true;
+    awscli.enable = true;
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    ripgrep.enable = true;
+    k9s.enable = true;
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
+  };
+
+
+  home.packages = with pkgs; [
+    azure-cli
+    bitwarden-cli
+    dig
+    tree
+    docker-compose
+    python311
+    poetry
+    rustc
+    cargo
+    go
+    yarn
+    cloc
+    lsof
+    cmake
+    llvmPackages.clangUseLLVM
+    coreutils
+    gnumake
+    nim
+    yq
+    jq
+    gnupg
+    rsync
+    vault
+    whois
+    sops
+    killall
+    openssl
+    ssh-to-age
+    ssh-to-pgp
+    shellcheck
   ];
 }
--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -13,65 +13,17 @@
   home.stateVersion = "23.11";
 
   home.packages = with pkgs; [
-    azure-cli
-    dig
-    tree
-    docker-compose
-    python311
-    poetry
-    rustc
-    cargo
-    go
-    yarn
     bitwarden
-    bitwarden-cli
     discord
     microsoft-edge
-    cloc
-    lsof
-    cmake
-    llvmPackages.clangUseLLVM
-    coreutils
-    gnumake
-    nim
-    yq
-    jq
     libsForQt5.kwalletmanager
     gnome.seahorse
-    dvc-with-remotes
-    gnupg
-    rsync
-    vault
-    whois
-    sops
-    killall
-    openssl
-    ssh-to-age
-    ssh-to-pgp
-    shellcheck
     synergy
     waynergy
     obs-studio
   ];
 
-  programs = {
-    htop.enable = true;
-    awscli.enable = true;
-    fzf = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-    ripgrep.enable = true;
-    k9s.enable = true;
-    direnv = {
-      enable = true;
-      enableZshIntegration = true;
-      nix-direnv.enable = true;
-    };
-  };
-
   home.sessionVariables = {};
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
