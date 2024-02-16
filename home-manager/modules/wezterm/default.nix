f5707c8182619978d629d7e3725609eb7a4c970c
Albin Vass <albinvass@gmail.com>
2024-02-16 13:34:26 +0100

    increase wezterm font size

--- home-manager/modules/wezterm/default.nix
+++ home-manager/modules/wezterm/default.nix
@@ -13,7 +13,7 @@
       config.window_background_opacity = 0.82
       config.hide_tab_bar_if_only_one_tab = true
       config.font = wezterm.font('FiraCode Nerd Font SemBd')
-      config.font_size = 9
+      config.font_size = 10
       return config
     '';
   };
