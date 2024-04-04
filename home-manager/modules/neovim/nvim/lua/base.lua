b187a123053475516f1231484d391b2dbcfbc100
Albin Vass <albinvass@gmail.com>
2024-04-04 09:29:57 +0200

    Add json syntax for cni conflists

--- home-manager/modules/neovim/nvim/lua/base.lua
+++ home-manager/modules/neovim/nvim/lua/base.lua
@@ -40,3 +40,5 @@ vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document
 vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
 vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
 vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+
+vim.filetype.add({ extension = { conflist = 'json' } }) 
