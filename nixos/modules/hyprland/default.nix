199bd43ae7f449af6b4b1efc2006dc0e4c0bebe8
Albin Vass <albinvass@gmail.com>
2024-05-01 14:40:31 +0200

    Enable experimental sddm wayland support

--- nixos/modules/hyprland/default.nix
+++ nixos/modules/hyprland/default.nix
@@ -12,7 +12,10 @@
     };
   };
 
-  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.login.enableGnomeKeyring = true;
   programs.hyprland.enable = true;
