bc4ec170f3893180869198eac76e3a24162a475a
Albin Vass <albinvass@gmail.com>
2024-06-11 20:13:24 +0200

    Set bash syntax for ansible shell tasks

--- home-manager/modules/neovim/nvim/after/queries/yaml/injections.scm
+++ home-manager/modules/neovim/nvim/after/queries/yaml/injections.scm
@@ -1,9 +1,21 @@
 ;; extends
-(block_mapping_pair
-  key: (flow_node)
-  value: (block_node
-      (block_scalar
-        (comment) @injection.language
-        (#gsub! @injection.language "#%s*([%w%p]+)%s*" "%1")
-        ) @injection.content)
+(block_sequence_item
+  (block_node
+    (block_mapping
+      (block_mapping_pair
+	key:
+	  (flow_node
+	    ((plain_scalar) @task_module
+		(#eq? @task_module
+		"shell")) @shell
+          )
+	value:
+	  (block_node
+	    (block_scalar) @injection.content
+	    (#set! injection.language "bash")
+	    (#set! injection.include-children)
+	  )
+      )
+    )
   )
+)
