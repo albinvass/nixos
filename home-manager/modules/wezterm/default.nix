3b6a645ecc79e6799eede3c74e3e0e249a2d10a6
Albin Vass <git@mail.albinvass.se>
2025-05-19 20:17:59 +0200

    Remove old wezterm workaround

--- home-manager/modules/wezterm/default.nix
+++ home-manager/modules/wezterm/default.nix
@@ -13,8 +13,6 @@
     home.file."${config.xdg.configHome}/wezterm" = {
       source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/git/github/albinvass/nixos/home-manager/modules/wezterm/config";
     };
-    # FIX See: https://github.com/nix-community/home-manager/issues/1807#issuecomment-1740960646
-    xdg.configFile."wezterm/wezterm.lua".enable = false;
     programs.wezterm = {
       enable = true;
       enableZshIntegration = true;
