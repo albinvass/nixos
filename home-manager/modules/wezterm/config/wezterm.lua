11576a71fc920099086ff6fedc50073397b7c1f5
Albin Vass <git@mail.albinvass.se>
2025-05-22 17:42:15 +0200

    wezterm: reduxe opacity

--- home-manager/modules/wezterm/config/wezterm.lua
+++ home-manager/modules/wezterm/config/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.99
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font('FiraCode Nerd Font SemBd')
 config.adjust_window_size_when_changing_font_size = false
