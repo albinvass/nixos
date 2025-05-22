439868ec333a2b522834bfe49a4c801053316100
Albin Vass <git@mail.albinvass.se>
2025-05-22 17:39:55 +0200

    Reduce font size in wezterm

--- home-manager/modules/wezterm/config/wezterm.lua
+++ home-manager/modules/wezterm/config/wezterm.lua
@@ -5,7 +5,7 @@ config.window_background_opacity = 0.95
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font('FiraCode Nerd Font SemBd')
 config.adjust_window_size_when_changing_font_size = false
-config.font_size = 10
+config.font_size = 9
 config.color_scheme = "Catppuccin Mocha"
 
 local act = wezterm.action
