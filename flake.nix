b2b4afdecdf48290f02a1d863f4e5537e7ad8f47
Albin Vass <git@mail.albinvass.com>
2025-07-24 16:02:59 +0200

    Remove unused devshell

--- flake.nix
+++ flake.nix
@@ -67,9 +67,6 @@
           '';
         };
       };
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = [ inputs.attic.packages.x86_64-linux.attic-client ];
-      };
       packages.x86_64-linux = import ./packages.nix { inherit inputs pkgs; };
       formatter.x86_64-linux = pkgs.nixfmt-tree;
       nixosConfigurations = self.lib.importHosts ./hosts {
