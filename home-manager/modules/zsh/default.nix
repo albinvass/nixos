91ff0718f2279541e19fbc9fb8f06e83b1c6a7b0
Albin Vass <git@mail.albinvass.com>
2025-07-24 15:24:55 +0200

    Remove unused zellij cruft

--- home-manager/modules/zsh/default.nix
+++ home-manager/modules/zsh/default.nix
@@ -45,20 +45,6 @@ in
             if [ -f "$HOME/.local-secrets" ]; then
               source "$HOME/.local-secrets"
             fi
-
-            zellij_tab_name_update() {
-              if [[ -n $ZELLIJ ]]; then
-                local current_dir=$PWD
-                if [[ $current_dir == $HOME ]]; then
-                    current_dir="~"
-                else
-                    current_dir=''${current_dir##*/}
-                fi
-                command nohup zellij action rename-tab $current_dir >/dev/null 2>&1
-              fi
-            }
-
-            chpwd_functions+=(zellij_tab_name_update)
           '';
         enableCompletion = false;
         autosuggestion.enable = true;
