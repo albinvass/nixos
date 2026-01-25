f2daa410df8655d0cd90a960f0971b6b0d135884
Albin Vass <github@albinvass.com>
2026-01-25 17:24:25 +0100

    Script to reformat all commits from given revision

---
+++ home-manager/modules/fish/functions/rebase-reformat.fish
@@ -0,0 +1,25 @@
+#!/usr/bin/env fish
+
+set revision $argv[1]
+set reformat_exec $argv[2]
+
+git rebase $revision \
+  --exec "\
+$reformat_exec && \
+git add -u && \
+git commit --allow-empty --fixup HEAD && \
+git revert --no-commit HEAD && \
+git commit --allow-empty --no-edit\
+"
+
+set VIM_DELETE_FIRST_COMMIT_MESSAGE "'g/^# This is the commit message #2:/1,.+1d'"
+set VIM_SAVE "'wq'"
+set VIM_DELETE_COMMENTS "'g/^#/d'"
+set VIM_DELETE_TWO_LAST_LINES "'normal! Gdk'"
+set VIM_PICK_TO_FIXUP "'g/^\w* \w* \(# \)\?fixup!/s/^pick/fixup/'"
+set VIM_MAKE_SQUASH_BELOW_REVERT "'g/^pick \w* \(# \)\?Revert \"fixup!/normal! j0ces'"
+
+env \
+  GIT_EDITOR="vim -es +$VIM_DELETE_FIRST_COMMIT_MESSAGE +$VIM_SAVE" \
+  GIT_SEQUENCE_EDITOR="vim -es +$VIM_DELETE_COMMENTS +$VIM_DELETE_TWO_LAST_LINES +$VIM_PICK_TO_FIXUP +$VIM_MAKE_SQUASH_BELOW_REVERT +$VIM_SAVE" \
+  git rebase -i $revision
