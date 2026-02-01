6e2c729e43bff972607b8dbccc79469215ad74cd
Albin Vass <github@albinvass.com>
2026-02-01 10:55:22 +0100

    rebase-reformat: run vim in noplugin mode

--- home-manager/modules/fish/functions/rebase-reformat.fish
+++ home-manager/modules/fish/functions/rebase-reformat.fish
@@ -20,6 +20,6 @@ set VIM_PICK_TO_FIXUP "'g/^\w* \w* \(# \)\?fixup!/s/^pick/fixup/'"
 set VIM_MAKE_SQUASH_BELOW_REVERT "'g/^pick \w* \(# \)\?Revert \"fixup!/normal! j0ces'"
 
 env \
-  GIT_EDITOR="vim -es +$VIM_DELETE_FIRST_COMMIT_MESSAGE +$VIM_SAVE" \
-  GIT_SEQUENCE_EDITOR="vim -es +$VIM_DELETE_COMMENTS +$VIM_DELETE_TWO_LAST_LINES +$VIM_PICK_TO_FIXUP +$VIM_MAKE_SQUASH_BELOW_REVERT +$VIM_SAVE" \
+  GIT_EDITOR="vim --noplugin -es +$VIM_DELETE_FIRST_COMMIT_MESSAGE +$VIM_SAVE" \
+  GIT_SEQUENCE_EDITOR="vim --noplugin -es +$VIM_DELETE_COMMENTS +$VIM_DELETE_TWO_LAST_LINES +$VIM_PICK_TO_FIXUP +$VIM_MAKE_SQUASH_BELOW_REVERT +$VIM_SAVE" \
   git rebase -i $revision
