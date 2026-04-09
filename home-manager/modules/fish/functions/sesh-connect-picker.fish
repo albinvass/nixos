3c93c77084ed378abf344243b342bc2b2b4d92be
Albin Vass <git@mail.albinvass.com>
2026-04-09 11:02:22 +0200

    Better sesh filter

--- home-manager/modules/fish/functions/sesh-connect-picker.fish
+++ home-manager/modules/fish/functions/sesh-connect-picker.fish
@@ -1,5 +1,5 @@
 function sesh-connect-picker
-    set -l session (tv --source-command "sesh-list" --source-output "{split: :1}" --no-preview --ansi)
+    set -l session (tv --source-command "fish -c sesh-list" --source-output "{split: :1}" --no-preview --ansi --no-sort)
     if test -n "$session"
         sesh connect $session
     end
--- home-manager/modules/fish/functions/sesh-list.fish
+++ home-manager/modules/fish/functions/sesh-list.fish
@@ -1,11 +1,32 @@
 function sesh-list
     set -l current (tmux display-message -p '#S')
-    for line in (sesh list -id | grep -v scratch)
+    set -l current_line
+    set -l tmux_sessions
+
+    # Tmux sessions from sesh (with icons, no zoxide)
+    for line in (sesh list -itd | grep -v scratch)
         set -l name (string split ' ' -- $line)[-1]
         if test "$name" = "$current"
-            echo "$line (current)"
+            set current_line "$line (current)"
         else
-            echo "$line"
+            set -a tmux_sessions "$line"
+        end
+    end
+
+    # Print current session first
+    if test -n "$current_line"
+        echo $current_line
+    end
+
+    # Print other tmux sessions
+    for line in $tmux_sessions
+        echo $line
+    end
+
+    # Git repos from zoxide
+    for d in (zoxide query -l)
+        if test -d "$d/.git"
+            echo $d
         end
     end
 end
