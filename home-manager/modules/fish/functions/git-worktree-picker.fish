0a0854099772b37f7fa78be4a60d405731e824d8
Albin Vass <git@mail.albinvass.com>
2026-04-09 11:27:02 +0200

    fix git-worktree keybind

--- home-manager/modules/fish/functions/git-worktree-picker.fish
+++ home-manager/modules/fish/functions/git-worktree-picker.fish
@@ -1,7 +1,7 @@
 function git-worktree-picker
-    set -l worktree (tv git-worktrees)
+    set -l worktree (tv --source-command "git worktree list --porcelain | grep '^worktree' | cut -d' ' -f2-" --preview-command "cd '{}' && git log --oneline -10 --color=always && echo && git status --short")
     if test -n "$worktree"
         cd $worktree
-        commandline -f repaint
     end
+    commandline -f repaint
 end
