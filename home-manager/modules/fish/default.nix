2287b10bfa4afae087e607429f6556948d8ced04
Albin Vass <git@mail.albinvass.com>
2026-05-04 13:05:47 +0200

    Stop using vim keybindings

--- home-manager/modules/fish/default.nix
+++ home-manager/modules/fish/default.nix
@@ -55,7 +55,6 @@ in
           gst = "git status";
         };
         shellInit = /* fish */ ''
-          set -g fish_key_bindings fish_vi_key_bindings
           bind ctrl-o git-worktree-picker
           bind -M insert ctrl-o git-worktree-picker
         '';
